c3ce56b1ceddc0fb4465e372f10f95f706d053c1
dmullan17 <43132178+dmullan17@users.noreply.github.com>
2020-09-19 16:59:01 +0100

    more

--- Models/GameWeek.cs
+++ Models/GameWeek.cs
@@ -7,7 +7,7 @@ namespace FPL.Models
     {
         public int id { get; set; }
 
-        public string Name { get; set; }
+        public string name { get; set; }
         public DateTime deadline_time { get; set; }
         public int average_entry_score { get; set; }
         public bool finished { get; set; }
