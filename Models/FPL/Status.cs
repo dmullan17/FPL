acc48a0debd82661de5123b148c0a26119646274
dmullan17 <43132178+dmullan17@users.noreply.github.com>
2021-11-28 18:02:45 +0000

    making random int nullable

--- Models/FPL/Status.cs
+++ Models/FPL/Status.cs
@@ -7,8 +7,8 @@ namespace FPL.Models.FPL
 {
     public class Status
     {
-        public int qualification_event { get; set; }
-        public int qualification_numbers { get; set; }
+        public int? qualification_event { get; set; }
+        public int? qualification_numbers { get; set; }
         public object qualification_rank { get; set; }
         public string qualification_state { get; set; }
     }
